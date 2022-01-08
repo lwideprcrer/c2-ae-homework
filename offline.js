c02cc5c978790594e913f112f68070033f59ba4c
creeper <55619104+lwideprcrer@users.noreply.github.com>
2022-01-09 04:35:13 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1641673652,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/firecracker-sheet0.png",
+		"images/firecracker-sheet1.png",
+		"images/fu-sheet0.png",
+		"images/lantern-sheet0.png",
+		"images/gnl-sheet0.png",
+		"images/eidk-sheet0.png",
+		"images/eidk-sheet1.png",
+		"media/spring_overture.ogg",
+		"media/explosion.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
